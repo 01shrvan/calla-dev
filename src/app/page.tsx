62beb2468c1aa33d5da4660ab9c147a20b956878
Shrvan Benke <154585738+pix-panther24@users.noreply.github.com>
2025-08-01 18:43:25 +0530

    feat: add new UI components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+"use client"
+
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
+
 const Page = () => {
+  const trpc = useTRPC();
+  const { data } = useQuery(trpc.createAI.queryOptions({ text: "nigga" }))
+
+  // localhost:3000/api/create-ai?body={text:"niggaaaaa"}
+
   return (
-    <div>hello nigga!
-    </div>
+    <div>{JSON.stringify(data)}</div>
   )
 }
 
