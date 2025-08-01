f12be9c631ee98a6a3481288d9eb4a4e414024ca
Shrvan Benke <154585738+pix-panther24@users.noreply.github.com>
2025-08-01 22:19:53 +0530

    refactor: remove unused useEffect and useState

--- src/app/client.tsx
+++ src/app/client.tsx
@@ -2,15 +2,10 @@
 
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
-import { useEffect, useState } from "react";
 
 export const Client = () => {
     const trpc = useTRPC();
     const { data } = useSuspenseQuery(trpc.createAI.queryOptions({ text: "shrvan" }))
-
-    // useEffect(() => {
-    //     const [] = useState();
-    // })
 
     return (
         <div>
