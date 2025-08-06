9cd76b54b34e56964385c3667d97bc13c955ab76
Shrvan Benke <154585738+pix-panther24@users.noreply.github.com>
2025-08-06 19:22:48 +0530

    feat: update Page component to use TRPC mutation

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -4,7 +4,12 @@ export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
+    await step.sleep("wait-a-moment", "30s");
+
     await step.sleep("wait-a-moment", "10s");
+
+    await step.sleep("wait-a-moment", "5s");
+
     return { message: `Hello ${event.data.email}!` };
   },
 );
