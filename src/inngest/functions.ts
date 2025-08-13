2f135597453008ee18a6dbd8de9be21da8434f2d
Shrvan Benke <154585738+pix-panther24@users.noreply.github.com>
2025-08-13 21:01:36 +0530

    feat: use e2b sandboxes to generate outputs with unique links per prompt

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,20 +1,34 @@
+import { Sandbox } from "@e2b/code-interpreter"
 import { gemini, createAgent } from "@inngest/agent-kit";
 
 import { inngest } from "./client";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
-     const codeAgent = createAgent({
+  async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("calla-dev");
+      return sandbox.sandboxId;
+    })
+
+    const codeAgent = createAgent({
       name: "code-agent",
       system: "You are an expert Next.js developer. You write readable, maintainable code. You write simple Next.js & React snippets.",
       model: gemini({ model: "gemini-1.5-flash", apiKey: process.env.GEMINI_API_KEY }),
     });
 
     const { output } = await codeAgent.run(
-  `Write the following snippet:${event.data.value}`,
-);
-    return { output };
+      `Write the following snippet:${event.data.value}`,
+    );
+
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+
+      return `https://${host}`
+    })
+    return { output, sandboxUrl };
   },
 );
